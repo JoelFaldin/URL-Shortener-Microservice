33004ec3f0c192e1e5a9b7903332b735f07ea4b6
JoelFaldin <joelfaldin@gmail.com>
2024-02-12 10:03:18 -0300

    feat: added app, checking for invalid urls

--- index.js
+++ index.js
@@ -2,6 +2,13 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const app = express();
+app.use(express.json());
+// DNS and url:
+const dns = require('dns');
+const urlParser = require('url')
+//Body parser:
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: true }));
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
@@ -10,15 +17,31 @@ app.use(cors());
 
 app.use('/public', express.static(`${process.cwd()}/public`));
 
-app.get('/', function(req, res) {
+app.get('/', (req, res) => {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
 // Your first API endpoint
-app.get('/api/hello', function(req, res) {
+app.get('/api/hello', (req, res) => {
   res.json({ greeting: 'hello API' });
 });
 
-app.listen(port, function() {
+app.post('/api/shorturl', (req, res) => {
+  // const newUrl = new URL(url)
+  // console.log(newUrl)
+  const url = req.body.url
+  
+  dns.lookup(urlParser.parse(url).hostname, async (error, url) => {
+    if (error || !url) {
+      res.json({ error: 'invalid url' })
+    } else {
+      res.json({ message: 'not invalid url' })
+    }
+  })
+  
+})
+
+app.listen(port, () => {
   console.log(`Listening on port ${port}`);
+  console.log(`http://localhost:${port}`);
 });
